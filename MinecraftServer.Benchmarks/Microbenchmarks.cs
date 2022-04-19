732fe14d7f397737301a1bf5cd2d7624abfeae44
Adam Chen <encodeous7@gmail.com>
2022-04-18 23:41:00 -0400

    Add Recyclable Memory Stream & Benchmarks

---
+++ MinecraftServer.Benchmarks/Microbenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using MinecraftServer.Packets;
+using MinecraftServer.Packets.Serverbound;
+using MinecraftServer.Packets.Serverbound.Data;
+
+namespace MinecraftServer.Benchmarks;
+
+[MemoryDiagnoser]
+public class Microbenchmarks
+{
+    private static readonly Packet TestPacket = Packet.GetPacket<CsHandshake>();
+    private static readonly CsHandshakePacketData TestPacketData = new (9999, "testing-server-ip", UInt16.MaxValue, 1);
+    private static readonly NetworkConnection NullOutput = new(Stream.Null);
+    
+    [Benchmark]
+    public Packet GenericGetPacketPerformance()
+    {
+        return Packet.GetPacket<CsHandshake>();
+    }
+    
+    [Benchmark]
+    public Packet DynamicGetPacketPerformance()
+    {
+        return Packet.GetPacket(TestPacket.Type, TestPacket.Side, TestPacket.Id);
+    }
+    
+    [Benchmark]
+    public async ValueTask SerializePacketPerformance()
+    {
+        await TestPacket.SendPacket(TestPacketData, NullOutput);
+    }
+}
