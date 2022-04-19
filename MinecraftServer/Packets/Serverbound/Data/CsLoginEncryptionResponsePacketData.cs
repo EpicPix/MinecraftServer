1756ebfd7b46cbda39834482c4ffaca1445fba24
Adam Chen <encodeous7@gmail.com>
2022-04-19 15:55:33 -0400

    Add login encryption packets

---
+++ MinecraftServer/Packets/Serverbound/Data/CsLoginEncryptionResponsePacketData.cs
@@ -0,0 +1,15 @@
+﻿namespace MinecraftServer.Packets.Serverbound.Data;
+
+public class CsLoginEncryptionResponsePacketData : PacketData
+{
+    public CsLoginEncryptionResponsePacketData(byte[] sharedSecret, byte[] verifyToken)
+    {
+        SharedSecret = sharedSecret;
+        VerifyToken = verifyToken;
+    }
+
+    public int SharedSecretLength => SharedSecret.Length;
+    public byte[] SharedSecret { get; }
+    public int VerifyTokenLength => VerifyToken.Length;
+    public byte[] VerifyToken { get; }
+}
