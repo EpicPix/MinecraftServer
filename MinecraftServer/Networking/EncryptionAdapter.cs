305776c9b655ea077a772bb2dc4c89c82cd726f6
Adam Chen <encodeous7@gmail.com>
2022-04-21 17:09:59 -0400

    Fix problem in sending encrypted packets.

--- MinecraftServer/Networking/EncryptionAdapter.cs
+++ MinecraftServer/Networking/EncryptionAdapter.cs
@@ -1,58 +1,66 @@
-﻿using System.Security.Cryptography;
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.IO;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Parameters;
 
 namespace MinecraftServer.Networking;
 
 public class EncryptionAdapter : DataAdapter
 {
-    public Stream ReadStream { get; }
-    public Stream WriteStream { get; }
+    private BufferedBlockCipher _encrypt;
+    private BufferedBlockCipher _decrypt;
 
-    private Aes GetInstance(byte[] key)
+    private BufferedBlockCipher GetInstance(byte[] key, bool encrypt)
     {
-        var aes = Aes.Create();
-        aes.Mode = CipherMode.CFB;
-        aes.Padding = PaddingMode.None;
-        aes.KeySize = 128;
-        aes.FeedbackSize = 8;
-        aes.Key = key;
-        aes.IV = key;
-        return aes;
+        BufferedBlockCipher cipher = new BufferedBlockCipher(new CfbBlockCipher(new AesEngine(), 8));
+        cipher.Init(encrypt, new ParametersWithIV(new KeyParameter(key), key));
+        return cipher;
     }
     public EncryptionAdapter(DataAdapter baseAdapter, byte[] key)
     {
-        ReadStream = new CryptoStream(baseAdapter, GetInstance(key).CreateDecryptor(), CryptoStreamMode.Read);
-        WriteStream = new CryptoStream(baseAdapter, GetInstance(key).CreateEncryptor(), CryptoStreamMode.Write);
+        _encrypt = GetInstance(key, true);
+        _decrypt = GetInstance(key, false);
         BaseAdapter = baseAdapter;
-    }
-
-    public override void Close()
-    {
-        ReadStream.Close();
-        WriteStream.Close();
-        base.Close();
-    }
-
-    public override async ValueTask DisposeAsync()
-    {
-        await ReadStream.DisposeAsync();
-        await WriteStream.DisposeAsync();
     }
 
     public override void Flush()
     {
-        ReadStream.Flush();
-        WriteStream.Flush();
+        BaseAdapter.Flush();
     }
 
     public DataAdapter BaseAdapter { get; }
 
-    public override ValueTask<int> ReadAsync(Memory<byte> buf, CancellationToken ct = default)
+    private void Transform(BufferedBlockCipher tra, ArraySegment<byte> a, ArraySegment<byte> b)
     {
-        return ReadStream.ReadAsync(buf, ct);
+        tra.ProcessBytes(a.Array, a.Offset, a.Count, 
+            b.Array, b.Offset);
     }
 
-    public override ValueTask WriteAsync(ReadOnlyMemory<byte> buf, CancellationToken ct = default)
+    public override async ValueTask<int> ReadAsync(Memory<byte> buf, CancellationToken ct = default)
     {
-        return WriteStream.WriteAsync(buf, ct);
+        int len = await BaseAdapter.ReadAsync(buf, ct);
+        if (len == 0) return 0;
+        if (!MemoryMarshal.TryGetArray<byte>(buf, out var seg))
+        {
+            throw new InvalidOperationException("How tf are u even using non-heap memory with an async method??");
+        }
+        using var tPlainText = PooledArray.Allocate(len);
+        Transform(_decrypt, seg.Slice(0, len), tPlainText.Data);
+        new Memory<byte>(tPlainText.Data.Array, tPlainText.Data.Offset, len).CopyTo(buf);
+        return len;
+    }
+
+    public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buf, CancellationToken ct = default)
+    {
+        if (!MemoryMarshal.TryGetArray(buf, out var seg))
+        {
+            throw new InvalidOperationException("How tf are u even using non-heap memory with an async method??");
+        }
+        using var tCipherText = PooledArray.Allocate(buf.Length);
+        Transform(_encrypt, seg, tCipherText.Data);
+        await BaseAdapter.WriteAsync(tCipherText.Data.Slice(0, buf.Length));
     }
 }
