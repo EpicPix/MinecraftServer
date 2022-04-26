5fb1bb31a7f87693705d4f8ca94ec4d6cbe0e2f4
EpicPix <epicpixminecraft@gmail.com>
2022-04-26 17:53:30 +0200

    Add BlockState Source Generator

---
+++ MinecraftServer.SourceGenerators/BlockStateGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Microsoft.CodeAnalysis;
+
+namespace MinecraftServer.SourceGenerators;
+
+[Generator]
+public class BlockStateGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        foreach (var file in context.AdditionalFiles)
+        {
+            if (Path.GetFileName(file.Path).Equals("blocks.json"))
+            {
+                context.AddSource("BlockState.g.cs", GenerateBlockStates(file.GetText().ToString()));
+            }
+        }
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        
+    }
+
+
+    public String GenerateBlockStates(string rawJson)
+    {
+        var json = JsonDocument.Parse(rawJson).RootElement;
+
+        var source = new StringBuilder();
+
+        source.AppendLine("// Auto-generated code");
+        source.AppendLine("using System;");
+        source.AppendLine();
+        source.AppendLine("namespace MinecraftServer.Data");
+        source.AppendLine("{");
+        source.AppendLine("    public partial class BlockState {");
+        source.AppendLine("        public static readonly IList<BlockState> States;");
+        source.AppendLine("        public static readonly BlockState Air;");
+        source.AppendLine("        public static BlockState GetBlockState(string id) {");
+        source.AppendLine("            switch(id)");
+        source.AppendLine("            {");
+        uint iid = 0;
+        foreach (var c in json.EnumerateObject())
+        {
+            // var alone = c.Value.GetProperty("states").GetArrayLength() == 1;
+            foreach (var state in c.Value.GetProperty("states").EnumerateArray())
+            {
+                
+                if (state.TryGetProperty("default", out var prop) && prop.GetBoolean())
+                {
+                    source.AppendLine($"                case \"{c.Name}\": return States[{iid}];");
+                }
+                // else
+                // {
+                //     source.AppendLine($"                case \"{c.Name},{state.GetProperty("id")}\": return States[{iid}];");
+                // }
+                iid++;
+            }
+        }
+        source.AppendLine("                default: return Air;");
+        source.AppendLine("            }");
+        source.AppendLine("        }");
+        source.AppendLine("        static BlockState() {");
+        source.AppendLine($"            var states = new BlockState[{iid}];");
+        uint id = 0;
+        foreach (var c in json.EnumerateObject())
+        {
+
+            foreach (var state in c.Value.GetProperty("states").EnumerateArray())
+            {
+
+                source.AppendLine($"            states[{id}] = new BlockState({state.GetProperty("id")});");
+                id++;
+            }
+        }
+        source.AppendLine("            States = Array.AsReadOnly<BlockState>(states);");
+        source.AppendLine("            Air = GetBlockState(\"minecraft:air\");");
+        source.AppendLine("        }");
+        source.AppendLine("    }");
+        source.AppendLine("}");
+        return source.ToString();
+    }
+}
--- MinecraftServer/Data/BlockState.cs
+++ MinecraftServer/Data/BlockState.cs
@@ -1,14 +1,13 @@
 namespace MinecraftServer.Data;
 
-public class BlockState
+public partial class BlockState
 {
-    // TODO Use a code generator and an array for all possible IDs, and maybe a big switch case for all possible String Id -> Numeric Id cases
-
-    internal BlockState()
-    {
-        
-    }
     
-    public static readonly BlockState Air = new();
+    public readonly uint Id;
+    
+    public BlockState(uint id)
+    {
+        Id = id;
+    }
 
 }
