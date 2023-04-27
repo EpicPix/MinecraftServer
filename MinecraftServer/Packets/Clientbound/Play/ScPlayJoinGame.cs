fc5aa0da45cbcf6661fd65c33d221c2e5770fb3d
EpicPix <epicpixminecraft@gmail.com>
2023-04-27 17:19:00 +0200

    Update `ScPlayJoinGame` packet

--- MinecraftServer/Packets/Clientbound/Play/ScPlayJoinGame.cs
+++ MinecraftServer/Packets/Clientbound/Play/ScPlayJoinGame.cs
@@ -9,7 +9,7 @@ public class ScPlayJoinGame : Packet<ScPlayJoinGame, ScPlayJoinGamePacketData>
 
     public override PacketType Type => PacketType.Play;
     public override PacketBound Bound => PacketBound.Client;
-    public override uint Id => 0x26;
+    public override uint Id => 0x28;
 
     public override ValueTask<PacketData> ReadPacket(DataAdapter stream)
     {
@@ -23,7 +23,7 @@ public class ScPlayJoinGame : Packet<ScPlayJoinGame, ScPlayJoinGamePacketData>
         await stream.WriteIntAsync(packet.EntityId);
         await stream.WriteBoolAsync(false);
         await stream.WriteByteAsync(1);
-        await stream.WriteByteAsync(1);
+        await stream.WriteByteAsync(0xff);
         await stream.WriteVarIntAsync(1);
         await stream.WriteStringAsync("minecraft:overworld", ushort.MaxValue); // identifier
 
@@ -38,7 +38,9 @@ public class ScPlayJoinGame : Packet<ScPlayJoinGame, ScPlayJoinGamePacketData>
                                     .SetString("name", "minecraft:overworld")
                                     .SetInteger("id", 0)
                                     .Set("element",
-                                        new NbtTagCompound()
+                                    new NbtTagCompound()
+                                            .SetInteger("monster_spawn_block_light_limit", 0)
+                                            .SetInteger("monster_spawn_light_level", 0)
                                             .SetByte("piglin_safe", 0)
                                             .SetByte("natural", 1)
                                             .SetFloat("ambient_light", 1)
@@ -68,7 +70,7 @@ public class ScPlayJoinGame : Packet<ScPlayJoinGame, ScPlayJoinGamePacketData>
                                 .SetInteger("id", 0)
                                 .Set("element", 
                                     new NbtTagCompound()
-                                        .SetString("precipitation", "none")
+                                        .SetInteger("has_precipitation", 0)
                                         .SetFloat("temperature", 0)
                                         .SetFloat("downfall", 0.5f)
                                         .SetString("category", "none")
@@ -86,7 +88,7 @@ public class ScPlayJoinGame : Packet<ScPlayJoinGame, ScPlayJoinGamePacketData>
                                 .SetInteger("id", 1)
                                 .Set("element", 
                                     new NbtTagCompound()
-                                        .SetString("precipitation", "none")
+                                        .SetInteger("has_precipitation", 0)
                                         .SetFloat("temperature", 0)
                                         .SetFloat("downfall", 0.5f)
                                         .SetString("category", "none")
@@ -103,24 +105,7 @@ public class ScPlayJoinGame : Packet<ScPlayJoinGame, ScPlayJoinGamePacketData>
             )
             .Write(stream);
         
-        await new NbtTagRoot()
-            .SetByte("piglin_safe", 0)
-            .SetByte("natural", 1)
-            .SetFloat("ambient_light", 1)
-            .SetString("infiniburn", "#minecraft:infiniburn_overworld")
-            .SetByte("respawn_anchor_works", 0)
-            .SetByte("has_skylight", 1)
-            .SetByte("bed_works", 1)
-            .SetString("effects", "minecraft:overworld")
-            .SetByte("has_raids", 0)
-            .SetInteger("min_y", 0)
-            .SetInteger("height", 256)
-            .SetInteger("logical_height", 256)
-            .SetDouble("coordinate_scale", 1)
-            .SetByte("ultrawarm", 0)
-            .SetByte("has_ceiling", 0)
-            .Write(stream);
-        
+        await stream.WriteStringAsync("minecraft:overworld", ushort.MaxValue); // identifier
         await stream.WriteStringAsync("minecraft:overworld", ushort.MaxValue); // identifier
         await stream.WriteUnsignedLongAsync(0);
         await stream.WriteVarIntAsync(80);
@@ -128,6 +113,7 @@ public class ScPlayJoinGame : Packet<ScPlayJoinGame, ScPlayJoinGamePacketData>
         await stream.WriteVarIntAsync(16);
         await stream.WriteBoolAsync(false);
         await stream.WriteBoolAsync(true);
+        await stream.WriteBoolAsync(false);
         await stream.WriteBoolAsync(false);
         await stream.WriteBoolAsync(false);
     }
